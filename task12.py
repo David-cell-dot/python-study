3ba8f441ae71a5e070731c8e2c5e4762749517c3
David wafula <wafuladavid1987@gmail.com>
2025-11-04 14:38:16 +0300

    task12

---
+++ task12.py
@@ -0,0 +1,12 @@
+#  using python Write a program that prints the largest of 4 inputs taken as input from a user.
+# from the four numberd,find the largest
+
+# get input  from the user
+number1= float(input("enter first number"))
+number2=float(input("enter second number:"))
+number3=float(input("enter third number:"))
+number4=float(input("enter the fouth number:"))
+
+# find the largest number from the in-built function
+largest=(number1,number2,number3,number4)
+print(f"the largest number is: [Largest]")
