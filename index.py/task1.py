f85ce615b918d57c38af7ee173fbb2979d0027fe
David wafula <wafuladavid1987@gmail.com>
2025-11-07 00:52:17 +0300

    my task

--- index.py/task1.py
+++ index.py/task1.py
@@ -11,7 +11,13 @@ area= base*height*0.5
 print(area)
 
 
-
+#Prompt the user for a number either on a form input or the terminal. Depending on whether the number is even or odd, display  either “odd” or “even” to the user.
+# Hint: how does an even / odd number react differently when divided by 2?
     
-    
+numb1=float(input("enter the num1"))
+numb2=float(input("enter the numb2"))
+if numb1%2==0:
+    print("even number")
+else:
+    print("odd numbers")
     
