afba5ca0e0cbbd3d829cb937b3e14af9d415daa0
David wafula <wafuladavid1987@gmail.com>
2025-11-07 00:03:03 +0300

    my first task

---
+++ index.py/task1.py
@@ -0,0 +1,17 @@
+#Write a program that prompts the user to enter the base and height of a triangle and returns its area.
+
+
+height=float(input("enter height"))
+base=float(input('enter base area'))
+Area=base*height*0.5
+
+def area_triangle(base,height):
+    return 0.5*base*height
+area= base*height*0.5
+print(area)
+
+
+
+    
+    
+    
