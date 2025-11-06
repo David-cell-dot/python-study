fa3c8264efb7d5b80d6c2fb43f3a57896463c1ca
David wafula <wafuladavid1987@gmail.com>
2025-11-06 22:44:16 +0300

    classtask

---
+++ index.py/functin.py
@@ -0,0 +1,19 @@
+# write a python  program to reverse a string
+word=input("string")
+reversed=input.reversed("string")
+print(reversed)
+
+def reversed_string(a,b):
+    reversed(a,b)
+    x=reversed(a,b)
+    return x
+print(x)
+
+
+
+
+
+    
+
+     
+
