fe5977e6f5127886f74ea4811b5d5c57f51160eb
David <wafuladavid1987@gmail.com>
2025-11-29 06:23:18 +0300

    myloop

---
+++ Testloop.py
@@ -0,0 +1,18 @@
+num=[0,1,2,3,4,5]
+count=0
+while(count<5):
+    print(count)
+    count+=1
+else:
+    print("count value reached %d" %(count))
+
+
+
+
+for i in range(1,10):
+    if (i%5==0):
+        break
+    print(i)
+else:
+    print("this is not printed because for loop is terminated because of break but not due to fail condition")
+
---
+++ math.py
@@ -0,0 +1,4 @@
+list1=[ 40.60,70,80]
+LIST2=[20,30,40,50]
+multiply=[]
+For list1,list2 in  zip list (list1,list2):env
